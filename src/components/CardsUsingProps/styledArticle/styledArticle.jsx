1313d59b17e44a88a2218bd4da6f08501d31520c
JulieKodehode <julie.kodehode@gmail.com>
2022-11-04 13:48:32 +0100

    Fixed StyledArticle and updated comments.

--- src/components/CardsUsingProps/styledArticle/styledArticle.jsx
+++ src/components/CardsUsingProps/styledArticle/styledArticle.jsx
@@ -1,17 +1,20 @@
+// Import library to get the styled
+import styled from "styled-components";
+
 // Library: if its reusable you can put them in own folder, if its just one comp then you can define them in the same comp to keep it local
 // Unsure where you went? Assigned a value but never used?
 
-// const styledArticle = styled.article`
-// 	margin: 1em;
+const styledArticle = styled.article`
+	margin: 1em;
 
-// 	background-color: yellow;
+	background-color: yellow;
 
-// 	a:hover {
-// 		background-color: green;
-// 	}
-// `;
+	a:hover {
+		background-color: green;
+	}
+`;
 
-// export default styledArticle;
+export default styledArticle;
 
 // Read more about component styling using 3rd party library
 // Need to make an example where this is working. Do it similarly to CardsUsingProps and then display it into App.js? I think Im missing something here. Look in Lars repo to see.
