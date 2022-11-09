1444cc74d2ecc26fb9ae70e2f9a6119d0df0638d
JulieKodehode <julie.kodehode@gmail.com>
2022-11-09 11:27:02 +0100

    Fixing the branches.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -19,10 +19,6 @@ import InfoCard from "../components/InfoCard/InfoCards.jsx";
 // Import from ./components/CarsUsingProps.jsx
 import CardsUsingProps from "../components/CardsUsingProps/CardsUsingProps.jsx";
 
-// . terminal directory paths (hey look in this folder), / intellisense suggestions (hey i want something from inside this folder, the more .. the longer you backtrack into folders (first-lesson > src > component > Main needs .../ or can be ./src/component/Main/Main.jsx)
-// Should be enough with import Name to get auto-completion
-import Main from "../components/Main/Main";
-
 // Only return one parent(div in this case), but as many children as you want. The div is sort of like body in html(?) and then the header is the section to be displayed on page (?)
 // Header is a custom component. And we can add a title property(props) to reduce open code(?) and display properties instead of lines of text.
 // READ more on properties
@@ -52,9 +48,6 @@ const Home = () => {
 				{/* Separate jsx and css from components/CardsUsingProps */}
 				{/* Using props title and paragraph to not write lines of code, see CardsUsingProps to see props inclosed in <tags> */}
 				<CardsUsingProps title="Some title" paragraph="Some paragraph" />
-
-				{/* Import export practice with data instead of hardcode */}
-				<Main />
 
 				{/* Using the Sign Component */}
 				<Sign />
