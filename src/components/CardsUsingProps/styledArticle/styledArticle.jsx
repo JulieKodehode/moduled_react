397fea113834bca590f7a195eebfb526d17a9a7d
JulieKodehode <julie.kodehode@gmail.com>
2022-11-10 11:15:15 +0100

    .

--- src/components/CardsUsingProps/styledArticle/styledArticle.jsx
+++ src/components/CardsUsingProps/styledArticle/styledArticle.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 // Library: if its reusable you can put them in own folder, if its just one comp then you can define them in the same comp to keep it local
 // Unsure where you went? Assigned a value but never used?
 
-const styledArticle = styled.article`
+const StyledArticle = styled.article`
 	margin: 1em;
 
 	background-color: yellow;
@@ -14,7 +14,7 @@ const styledArticle = styled.article`
 	}
 `;
 
-export default styledArticle;
+export default StyledArticle;
 
 // Read more about component styling using 3rd party library
 // Need to make an example where this is working. Do it similarly to CardsUsingProps and then display it into App.js? I think Im missing something here. Look in Lars repo to see.
