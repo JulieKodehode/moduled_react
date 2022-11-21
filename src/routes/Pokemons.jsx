d2b82a51b94ff05a12b490acf88fe0d1e5206bc9
JulieKodehode <julie.kodehode@gmail.com>
2022-11-21 22:22:15 +0100

    Working apifetch and browser display

--- src/routes/Pokemons.jsx
+++ src/routes/Pokemons.jsx
@@ -1,11 +1,13 @@
 import FetchPokemon from "./Pokemons/FetchPokemon";
+import GetPokemon from "./Pokemons/FetchPokemon";
 
 const Pokemons = () => {
 	return (
 		// className inserted for background and center, no additional css.
 		<div className="App | App-header">
-			<h1>Pokemons are cool</h1>
-			<FetchPokemon />
+			{/* <h1>Pokemons are cool</h1> */}
+			{/* <FetchPokemon /> */}
+			<GetPokemon />
 		</div>
 	);
 };
--- src/routes/Pokemons/FetchPokemon.jsx
+++ src/routes/Pokemons/FetchPokemon.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
 
-async function FetchPokemon(callback) {
-	const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto"); /* Modify url */
-	// console.log(response);
-	const data = await response.json();
-	// console.log(data);
-	// console.log(data.name);
+// Bitch ain't working outside
+// async function FetchPokemon(callback) {
+// 	const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto"); /* Modify url */
+// 	// console.log(response);
+// 	const data = await response.json();
+// 	// console.log(data);
+// 	// console.log(data.name);
 
-	callback(data.name);
-}
+// 	callback(data.name);
+// }
 
 console.log("U WOKRING BRO??");
 
@@ -16,31 +17,31 @@ const GetPokemon = () => {
 	const [data, setData] = useState(null);
 
 	useEffect(() => {
-		console.log("useEffect was run");
+		async function getData() {
+			const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+			const data = await response.json();
 
-		FetchPokemon(setData);
+			// console.log(data.name);
+			// console.error(data);
+			setData(data.name);
+		}
+
+		getData();
+		// console.log("useEffect was run");
+
+		// FetchPokemon(setData);
 		// console.log(getPokeApi); // function
 		// console.log(setPokeApi); // unidentified
 	}, []);
 
 	return (
 		<div>
-			<p>something</p>
-			<>
-				{data &&
-					data.map(() => {
-						return (
-							<div>
-								<h1 key="name">{data.name}</h1>
-								<img key="sprites.default_front">{data.sprites.default_front}</img>
-							</div>
-						);
-					})}
-			</>
+			{/* <p>something</p> */}
+			<h1>{data}</h1>
 		</div>
 	);
 };
 
 // console.log(FetchPokemon);
 
-export default FetchPokemon;
+export default GetPokemon;
