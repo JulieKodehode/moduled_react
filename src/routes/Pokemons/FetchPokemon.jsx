62ca49007851e45ea2af435eef5b7052ca147648
JulieKodehode <julie.kodehode@gmail.com>
2022-11-24 13:30:43 +0100

    Updated FetchPokemon to fetch an img url

--- src/routes/Pokemons/FetchPokemon.jsx
+++ src/routes/Pokemons/FetchPokemon.jsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react";
 
-console.log("U WOKRING BRO??");
+// console.log("U WORKING BRO??");
 
 const GetPokemon = () => {
 	const [data, setData] = useState(null);
 
 	useEffect(() => {
 		async function getData() {
-			const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+			const response = await fetch("https://pokeapi.co/api/v2/pokemon/snorlax");
 			console.log(response);
 			const data = await response.json();
 			console.log(data);
 
 			// console.log(data.name);
 			// console.error(data);
-			setData(data.name, data.sprites.default_front);
+			setData(data);
 		}
 
 		getData();
@@ -26,10 +26,13 @@ const GetPokemon = () => {
 	}, []);
 
 	return (
-		<div>
+		<div style={{ border: "2px solid #111", padding: "10px" }}>
 			{/* <p>something</p> */}
-			<h1>{data}</h1>
-			<img src={data.sprites.front_default} />
+			<h1>{data && data.name}</h1>
+
+			{data && data.sprites.front_default && (
+				<img src={data.sprites.front_default} alt="snorlax the pokemon" />
+			)}
 		</div>
 	);
 };
